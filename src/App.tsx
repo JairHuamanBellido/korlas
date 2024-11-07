83e3165f8714fb18c253f7cd599783553ebd7ea9
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2024-11-07 10:45:41 -0500

    feat: single session detector (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,29 @@ import { useEffect } from "react";
 import { MaterialsInventoryService } from "./domain/services/MaterialsInventoryService";
 import { FlowGraphService } from "./domain/services/FlowGraphService";
 import { ResourcesInventoryService } from "./domain/services/ResourcesInventoryService";
+import useSingleSession from "./hooks/useSingleSession";
+import DisconnectedContainer from "./components/disconnect/disconnected-container";
 
 function App() {
   const flowGraph = useLiveQuery(() => FlowGraphService.getCurrent())!;
-
+  const isActiveSession = useSingleSession();
   useEffect(() => {
     MaterialsInventoryService.create();
-    ResourcesInventoryService.create()
+    ResourcesInventoryService.create();
     FlowGraphService.create();
   }, []);
 
-  return (
-    <div className="w-full h-full relative flex">
-      <Sidebar />
-      {!!flowGraph && <Board flowGraph={flowGraph} />}
-      <NodeInfoSidebar />
-    </div>
-  );
+  if (isActiveSession) {
+    return (
+      <div className="w-full h-full relative flex">
+        <Sidebar />
+        {!!flowGraph && <Board flowGraph={flowGraph} />}
+        <NodeInfoSidebar />
+      </div>
+    );
+  } else {
+    return <DisconnectedContainer />;
+  }
 }
 
 export default App;
---
+++ src/hooks/useSingleSession.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+function useSingleSession() {
+  const [isActiveSession, setIsActiveSession] = useState(true);
+  const currentTabId = sessionStorage.getItem("tabId") || Date.now().toString();
+  sessionStorage.setItem("tabId", currentTabId);
+
+  useEffect(() => {
+    localStorage.setItem("lastActiveTab", currentTabId);
+
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === "lastActiveTab") {
+        const lastActiveTab = localStorage.getItem("lastActiveTab");
+
+        setIsActiveSession(lastActiveTab === currentTabId);
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [currentTabId]);
+
+  return isActiveSession;
+}
+
+export default useSingleSession;
